d06cad7883db94ed5bd16765cab58d0339208525
adikub <adiletkub@gmail.com>
2020-02-11 11:22:17 +0600

    added projecys

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -3,9 +3,57 @@ import React from 'react'
 import './styles.scss';
 
 const Projects = () => {
+  const projects = {
+    'node_modules': ['closedFolder', 1],
+    'src': ['openedFolder', 1],
+    'assets': ['closedFolder', 2],
+    'components': ['openedFolder', 2],
+    'Content': ['openedFolder', 3],
+    'index.jsx': ['jsx', 4],
+    'styles.scss': ['scss', 4],
+    'Footer': ['closedFolder', 3],
+    'Header': ['closedFolder', 3],
+    'Projects': ['closedFolder', 3],
+    'SideBar': ['closedFolder', 3],
+    'Toolbar':['closedFolder', 3],
+    'babel.js': ['js', 2],
+    'index.js': ['jsx', 2],
+    '.gitignore': ['git', 2],
+    'package-lock.json': ['json', 1],
+    'package.json': ['json', 1],
+    'webpack.config.js': ['js', 1]
+  }
+  const setLogo = (format) => {
+    if (format[0] === 'closedFolder' || format[0] === 'openedFolder' ) {
+      return (
+        <div
+          style={{
+            transform: `${format[0] === 'closedFolder' ? 'rotate(45deg)' : 'rotate(135deg)'}`
+          }}
+         className='sidebar-item_arrow sidebar-item_arrow__small'
+        >
+        </div>
+      )
+    }
+    
+  }
   return (
     <div className="project">
-      
+      {Object.keys(projects).map((key) => {
+        return (
+          <div
+            id={key}
+            key={key}
+            style={{paddingLeft: 10*projects[key][1]}}
+            className='sidebar-item-content_title'>
+              {setLogo(projects[key])}   
+              <span className='sidebar-item_text'>
+                {key}
+              </span>
+          </div>
+        )
+      })}
+       
     </div>
   )
 }
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,85 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
+import Projects from '../Projects'
 import './styles.scss';
 
 const SideBar = () => {
-  const [open, isOpen]=useState(true)
+  const [open, isOpen] = useState(true)
   const sidebarName = ['OPEN EDITORS', 'fakevscode', 'outline', 'npm scripts']
-  
+
   return (
     <div className="sidebar">
       <div className='sidebar-explorer'>
         <span>explorer</span>
       </div>
-      {sidebarName.map((text) =>
-        <div
-          className='sidebar-item'
-          id={text}
-          key={text}>
+      {sidebarName.map((text) => {
+        const fakevscode = 'fakevscode' === text
+        return (
           <div
-            onClick={()=>{'fakevscode' === text && isOpen(!open)}} 
-            className='sidebar-item-title'>
+            className='sidebar-item'
+            id={text}
+            key={text}>
             <div
-              style={{transform: `rotate(${'fakevscode' === text && open ? '135deg' : '45deg'})`}} 
-              className='sidebar-item_arrow'>
+              onClick={() => { fakevscode && isOpen(!open) }}
+              className='sidebar-item-title'>
+              <div
+                style={{ transform: `rotate(${fakevscode && open ? '135deg' : '45deg'})` }}
+                className='sidebar-item_arrow'>
+              </div>
+              <span className='sidebar-item_text'>
+                {text.toUpperCase()}
+              </span>
             </div>
-            <span className='sidebar-item_text'>
-              {text.toUpperCase()}
-            </span>
+            <div
+              style={{ height: fakevscode && open ? '550px' : '0px' }}
+              className='sidebar-item-content'>
+              {fakevscode && <Projects />}
+            </div>
           </div>
-          <div
-            style={{height: 'fakevscode' === text && open ? '550px' : '0px'}}
-            className='sidebar-item-content'>
-            
-            <div className='sidebar-item-content_title'>
-              <div
-                style={{transform: `rotate(45deg)`}} 
-                className='sidebar-item_arrow sidebar-item_arrow__small'>
-              </div>
-              <span className='sidebar-item_text'>
-                {text}
-              </span>
-            </div>
-            <div className='sidebar-item-content_title'>
-              <div
-                style={{transform: `rotate(45deg)`}} 
-                className='sidebar-item_arrow sidebar-item_arrow__small'>
-              </div>
-              <span className='sidebar-item_text'>
-                {text}
-              </span>
-            </div>
-            <div className='sidebar-item-content_title'>
-              <div
-                style={{transform: `rotate(45deg)`}} 
-                className='sidebar-item_arrow sidebar-item_arrow__small'>
-              </div>
-              <span className='sidebar-item_text'>
-                {text}
-              </span>
-            </div>
-            <div className='sidebar-item-content_title'>
-              <div
-                style={{transform: `rotate(45deg)`}} 
-                className='sidebar-item_arrow sidebar-item_arrow__small'>
-              </div>
-              <span className='sidebar-item_text'>
-                {text}
-              </span>
-            </div>
-            <div className='sidebar-item-content_title'>
-              <div
-                style={{transform: `rotate(45deg)`}} 
-                className='sidebar-item_arrow sidebar-item_arrow__small'>
-              </div>
-              <span className='sidebar-item_text'>
-                {text}
-              </span>
-            </div>
-        
-          </div>
-        </div>
-      )}
+        )
+      })}
     </div>
   )
 }
